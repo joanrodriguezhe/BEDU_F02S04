9b2a26d97613edaf63ff301234a8d2ebd9797068
Jose Rodriguez <shepe_rodriguez@hotmail.com>
2021-01-18 19:17:36 -0700

    Reto 01

--- main.js
+++ main.js
@@ -8,4 +8,14 @@ function multiplyByTwo(numbers) {
     return multipliedNumbers;
 }
 
-console.log(multiplyByTwo([1, 3, 4, 7, 2, 1, 9, 0]));
+//console.log(multiplyByTwo([1, 3, 4, 7, 2, 1, 9, 0]));
+
+//Reto 01
+function calculateAverage(numbers) {
+    let sum=numbers.reduce((el,nxt)=>{
+        return el+nxt
+    },0)
+    return sum/numbers.length;
+  }
+
+  console.log(calculateAverage([1,2,3,4,5]));
